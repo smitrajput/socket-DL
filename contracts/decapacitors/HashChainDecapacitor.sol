4c029116b11e6c6c5abc1c7145cd96613deb65a4
smitrajput <smitrajputtd@gmail.com>
2023-06-15 19:03:54 +0530

    🏌️ Optimize HashChainDecapacitor.verifyMessageInclusion()

--- contracts/decapacitors/HashChainDecapacitor.sol
+++ contracts/decapacitors/HashChainDecapacitor.sol
@@ -34,14 +34,35 @@ contract HashChainDecapacitor is IDecapacitor, AccessControl {
         bytes calldata proof_
     ) external pure override returns (bool) {
         bytes32[] memory chain = abi.decode(proof_, (bytes32[]));
-        uint256 len = chain.length;
         bytes32 generatedRoot;
         bool isIncluded;
-        for (uint256 i = 0; i < len; i++) {
-            generatedRoot = keccak256(abi.encode(generatedRoot, chain[i]));
-            if (chain[i] == packedMessage_) isIncluded = true;
+        /// @solidity memory-safe-assembly
+        assembly {
+            if mload(chain) {
+                // Initialize `offset` to the offset of `chain` elements in memory.
+                let offset := add(chain, 0x20)
+                // Left shift by 5 is equivalent to multiplying by 0x20.
+                // finding the position of the end of the array by adding chain's length's size to offset.
+                let end := add(offset, shl(5, mload(chain)))
+                // Iterate over chain elements to compute root hash.
+                for {} 1 {} {
+                    // Store elements to hash contiguously in scratch space.
+                    // Scratch space is 64 bytes (0x00 - 0x3f) and both elements are 32 bytes.
+                    mstore(0x00, generatedRoot)
+                    mstore(0x20, mload(offset))
+                    // generatedRoot = keccak256(abi.encode(generatedRoot, chain[i]));
+                    generatedRoot := keccak256(0x00, 0x40)
+                    // if (chain[i] == packedMessage_) isIncluded = true;
+                    if eq(mload(offset), packedMessage_) {
+                        isIncluded := true
+                    }
+                    // i++
+                    offset := add(offset, 0x20)
+                    // i < len
+                    if iszero(lt(offset, end)) { break }
+                }
+            }
         }
-
         return root_ == generatedRoot && isIncluded;
     }
 
